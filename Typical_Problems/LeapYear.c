69cff3e1a8efac412a9813d8316c96bce80b7b8b
Ayan Kumar Dey <ayankumardey99@gmail.com>
2024-06-02 00:09:02 +0530

    Leap Year @AyanKD C

---
+++ Typical_Problems/LeapYear.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int year;
+    printf("Enter the year:-\n");
+    scanf("%d",&year);
+    if(year%4==0)
+    {
+        if(year%100==0)
+        {
+            if(year%400==0)
+            {
+                printf("The year is a Leap Year");
+            }
+            else
+            {
+                printf("Th year is not a Leap Year");
+            }
+        }
+        else
+        {
+            printf("The year is a Leap Year");
+        }
+    }
+    else
+    {
+        printf("The year is not a Leap Year");
+    }
+    return 0;
+}
