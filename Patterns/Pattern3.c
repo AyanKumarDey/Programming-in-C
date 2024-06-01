ba19251a293fbcec0f8b0ad0fe0024b4b2228949
Ayan Kumar Dey <ayankumardey99@gmail.com>
2024-06-01 20:36:35 +0530

    Star Pattern3 @AyanKD C

---
+++ Patterns/Pattern3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the length of the pattern:-\n");
+    scanf("%d",&n);
+    printf("The following pattern is:-\n");
+    for(int i=0;i<n;i++)
+    {
+        for(int j=n;j>i;j--)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
