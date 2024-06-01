5f05b77cc4111f9a0c639920c422b5d724b16fc2
Ayan Kumar Dey <112239058+AyanKumarDey@users.noreply.github.com>
2024-06-01 17:25:40 +0530

    Update Pattern1.c

--- Patterns/Pattern1.c
+++ Patterns/Pattern1.c
@@ -4,6 +4,7 @@ int main()
     int n;
     printf("Enter the length of the row:-\n");
     scanf("%d",&n);
+    printf("The following pattern is:-\n");
     for(int i=0;i<n;i++)
     {
         for(int j=0;j<n;j++)
